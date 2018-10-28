86cbdaa0729f4acc91186dd8fd4e59d5ac205036
Max Lowery <Sass-Meister@users.noreply.github.com>
2018-10-28 18:05:46 -0500

    Renamed parser.py

--- CmdParser.py
+++ CmdParser.py
@@ -3,18 +3,20 @@
 # import userManager
 # import authManager
 
-# Each function has one mandatory field command which contains the string of the command to be executed and two optional
-# fields. One for a user which is not needed in all cases but if a user is not provided then the parser assumes that one
-# is not logged in and defaults to that permissions level. The other field is for testing this class and if set to true
-# does not interact with any API or manager but instead returns the method it would've with the parameters it would've
-# used. This is the best way I can figure out how to test this without turning this into an integration test and
-# validating that the parser works based on the APIs or the database. At this point and time (sprint 1) if testing is
-# true, then the user field does not matter (because that would test the auth manager). Hopefully this does not break
-# any scrum/agile stuff. The testing field could be a huge security flaw if left in but whatever.
+# This is the command parser class, used to bridge the gap between the CLI and our managers so the managers are able to
+# work with the website version (or whatever is next). This class has only static methods and only one public function,
+# parse. Each function has one mandatory field command which contains the string of the command to be executed and two
+# optional fields. One for a user which is not needed in all cases but if a user is not provided then the parser assumes
+# that one is not logged in and defaults to that permissions level. The other field is for testing this class and if set
+# to true does not interact with any API or manager but instead returns the method it would've with the parameters it
+# would've used. This is the best way I can figure out how to test this without turning this into an integration test
+# and validating that the parser works based on the APIs or the database. At this point and time (sprint 1) if the
+# testing field is true, then the user field does not matter (because checking the auth would become partly integration
+# testing with the auth manager). Hopefully this does not break any scrum/agile stuff. The testing field could be a huge
+# security flaw if left in but for now it's my best solution.
 
 
 class CommandParser:
-
     # These two data sets contain the all the commands and their description. The first array is useful for testing if
     # the parse function has been passed a valid command and the descriptions are useful for help and telling the user
     # when they messed up a command call.
