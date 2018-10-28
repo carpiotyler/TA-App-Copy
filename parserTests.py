763c37b421d593043dab73bd8358d7f594bc1b2b
Max Lowery <Sass-Meister@users.noreply.github.com>
2018-10-28 18:06:02 -0500

    Finished my tests

--- parserTests.py
+++ parserTests.py
@@ -1,67 +1,102 @@
 import unittest
-from parser import CommandParser as cp
+from CmdParser import CommandParser as p
 
-# This class contains the unit tests for the command parser check parser.py for better descriptions of how the functions
-# work. A useful thing to note is that the parse function has 3 fields, the command string, the optional user object,
-# and the option testing boolean. If testing is True, then the user does not matter. The parse function is the only one
-# tested by this class because the rest of the helper functions should be treated as private.
+# This class contains the unit tests for the command parser check CmdParser.py for better descriptions of how the
+# functions work. A useful thing to note is that the parse function has 3 fields, the command string, the optional user
+# object, and the option testing boolean. If testing is True, then the user does not matter. The parse function is the
+# only one tested by this class because the rest of the helper functions should be treated as private.
 
 
 class ParserTest(unittest.TestCase):
     def setUp(self):
-        cp.uting = True
+        p.uting = True
 
     def tearDown(self):
-        cp.uting = False
+        p.uting = False
 
     def test_courseAdd(self):
-        pass
+        self.assertEqual('%s.add(dept="CS", cnum="550")' % p.namesdict['course'],
+                         p.parse("course add dept=CS cnum=550"))
 
-    def test_courseEdit(self):
-        pass
+    def test_courseEditByCode(self):
+        self.assertEqual('%s.edit(code="CS-550", TA="Danny"' % p.namesdict['course'],
+                         p.parse("course edit code=CS-550 TA=Danny"))
 
-    def test_courseView(self):
-        pass
+    def test_courseEditByNums(self):
+        self.assertEqual('%s.edit(dnum="CS", cnum="550", TA="Danny"' % p.namesdict['course'],
+                         p.parse("course edit dnum=CS cnum=550 TA=Danny"))
 
-    def test_courseDelete(self):
-        pass
+    def test_courseViewAll(self):
+        self.assertEqual('%s.view()' % p.namesdict['course'],
+                         p.parse("course view"))
+
+    def test_courseViewOneByCode(self):
+        self.assertEqual(('%s.view(code="CS-351")' % p.namesdict['course']),
+                         p.parse("course view CS-351"))
+
+    def test_courseViewOneByNums(self):
+        self.assertEqual(('%s.view(dnum="CS", cnum="351")' % p.namesdict['course']),
+                         p.parse("course view dnum=CS cnum=351"))
+
+    def test_courseDeleteByCode(self):
+        self.assertEqual('%s.delete(code="CS-351")' % p.namesdict['course'],
+                         p.parse("course delete CS-351"))
+
+    def test_courseDeleteByNums(self):
+        self.assertEqual('%s.delete(dnum="CS", cnum="351")' % p.namesdict['course'],
+                         p.parse("course delete CS-351"))
 
     def test_sectionAdd(self):
-        pass
+        self.assertEqual('%s.add(dept="CS", cnum="550", snum="601")' % p.namesdict['section'],
+                         p.parse("section add dept=CS cnum=550 snum=601"))
 
-    def test_sectionEdit(self):
-        pass
+    def test_sectionEditByCode(self):
+        self.assertEqual('%s.edit(code="CS-550-601", TA="Danny"' % p.namesdict['section'],
+                         p.parse("section edit code=CS-550-601 TA=Danny"))
 
-    def test_sectionView(self):
-        pass
+    def test_sectionEditByNums(self):
+        self.assertEqual('%s.edit(dnum="CS", cnum="550", snum="601", TA="Danny"' % p.namesdict['section'],
+                         p.parse("section edit dnum=CS cnum=550 snum=601 TA=Danny"))
 
-    def test_sectionDelete(self):
-        pass
+    def test_sectionViewOneByCode(self):
+        self.assertEqual(('%s.view(code="CS-351-601")' % p.namesdict['section']),
+                         p.parse("section view CS-351"))
+
+    def test_sectionViewOneByNums(self):
+        self.assertEqual(('%s.view(dnum="CS", cnum="550", snum="601")' % p.namesdict['section']),
+                         p.parse("section view dnum=CS cnum=550 snum=601"))
+
+    def test_sectionViewAll(self):
+        self.assertEqual('%s.view()' % p.namesdict['section'],
+                         p.parse("section view"))
+
+    def test_sectionDeleteByCode(self):
+        self.assertEqual('%s.delete(code="CS-351-601")' % p.namesdict['section'],
+                         p.parse("section delete CS-351"))
+
+    def test_sectionDeleteByNums(self):
+        self.assertEqual('%s.delete(dnum="CS", cnum="550", snum="601")' % p.namesdict['section'],
+                         p.parse("section delete dnum=CS cnum=550 snum=601"))
 
     def test_userAdd(self):
-        pass
+        self.assertEqual('%s.add(username="Danny", password="1234"' % p.namesdict['user'],
+                         p.parse("user add username=Danny password=1234"))
 
     def test_userEdit(self):
-        pass
+        self.assertEqual('%s.edit(username="Danny", role="TA"' % p.namesdict['user'],
+                         p.parse("user edit username=Danny role=TA"))
 
-    def test_userView(self):
-        pass
+    def test_userViewOne(self):
+        self.assertEqual('%s.view(username="Danny")' % p.namesdict['user'],
+                         p.parse("user view username=Danny"))
+
+    def test_userViewAll(self):
+        self.assertEqual('%s.view()' % p.namesdict['user'],
+                         p.parse("user view"))
 
     def test_userDelete(self):
-        pass
-
-    def test_departmentAdd(self):
-        pass
-
-    def test_departmentEdit(self):
-        pass
-
-    def test_departmentView(self):
-        pass
-
-    def test_departmentDelete(self):
-        pass
-
+        self.assertEqual('%s.delete(username="Danny")' % p.namesdict['user'],
+                         p.parse("user delete username=Danny"))
 
 if __name__ == '__main__':  # Just a placeholder until a real test runner is written
     unittest.main()
