313d009caffcd4a6587b7b287d7cc64d43c4231b
Tyler Carpio <tkcarpio@uwm.edu>
2018-10-28 15:47:07 -0500

    Reworked AbstractStorageManager, should be viable to extend now.

--- StorageManager/AbstractStorageManager.py
+++ StorageManager/AbstractStorageManager.py
@@ -1,43 +1,57 @@
 from abc import ABC, abstractmethod
-from enum import Enum
 
-
-# The following Enums are used for interacting with arrays returned by AbstractStorageManager
-class Course(Enum):
-    DEPT = 0
-    CNUM = 1
-    SECTIONS = 2
-    NAME = 3
-    DESCRIPTION = 4
-
-
-class User(Enum):
-    USERNAME = 0
-    PASSWORD = 1
-    ROLE = 2
 
 class AbstractStorageManager(ABC):
-    """
-    List of functional storage methods to be implemented by a data system
-    """
+    # ################################################################################# #
+    # The following classes are used for interacting with AbstractStorageManager        #
+    # ################################################################################# #
+    class Course:
+        dept = ""
+        cnum = ""
+        sections = []
+        name = ""
+        description = ""
 
+    class User:
+        username = ""
+        password = ""
+        role = ""
 
+    class Section:
+        dept = ""
+        cnum = ""
+        snum = ""
+
+    # ################################################################################# #
+    # List of functional storage methods to be implemented by a data system             #
+    # ################################################################################# #
     @abstractmethod
     def set_up(self): pass
-    # Sets up storage
+    # Sets up storage. JSON, Database, whatever. Also creates one user: username=supervisor password=1234 role=supervisor
 
     @abstractmethod
-    def add_course(self, dept, cnum): pass
-    # Adds a course to the course list
+    def insert_course(self, course): pass
+    # Inserts a Course to the course list, overwriting if it exists.
+    # Does not validate data! Simply makes sure it has received a course object then inserts it to the database.
 
     @abstractmethod
-    def add_user(self, username, password): pass
-    # Adds a user to the user list
+    def insert_user(self, user): pass
+    # Inserts a User to the user list, overwriting if it exists. Does not error check for valid data!
+    # Does not validate data! Simply makes sure it has received a user object then inserts it to the database.
+
+    @abstractmethod
+    def insert_section(self, section): pass
+    # Inserts a Section to the user list, overwriting if it exists. Does not error check for valid data!
+    # Does not validate data! Simply makes sure it has received a user object then inserts it to the database.
 
     @abstractmethod
     def get_course(self, dept, cnum): pass
-    # returns an array representing the course requested, or None if nothing
+    # Builds a Course object that has this dept and cnum (plus other data from database) and returns it. None if no entry matches.
 
     @abstractmethod
     def get_user(self, username): pass
-    # Returns an array representing the user requested, or None if nothing
+    # Builds a User object that has this username (plus other data from database) and returns it. None if no entry matches
+
+    @abstractmethod
+    def get_section(self, dept, cnum, snum): pass
+    # Builds a Section object that has this username (plus other data from database) and returns it. None if no entry matches
--- StorageManager/StorageManagerTester.py
+++ StorageManager/StorageManagerTester.py
@@ -1,6 +1,1 @@
-from StorageManager.AbstractStorageManager import Course, User, AbstractStorageManager
-
-testCourse = ["CS", "351", ["401", "801"], "Data Structures and Algorithms", "Hard class"]
-
-print(testCourse[Course.SECTIONS.value])
-#Will return "351'
+pass
