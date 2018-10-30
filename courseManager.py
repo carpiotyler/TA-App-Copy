eb9d9b2f04cfb9d5ca643a487229de9b5b8789ff
Jas <singh87@uwm.edu>
2018-10-30 00:33:34 -0500

    Initial commit: Created skeleton Course Manager and started writing acceptance tests

---
+++ courseManager.py
@@ -0,0 +1,13 @@
+class CourseManager:
+
+    def add(self,course):
+        pass
+
+    def delete(self,course):
+        pass
+
+    def edit(self,course):
+        pass
+        
+    def view(self,course):
+        pass     
---
+++ courseTests.py
@@ -0,0 +1,12 @@
+import unittest
+from courseManager import CourseManager
+
+class CourseTest(unittest.TestCase):
+    def setUp(self):
+        c = CourseManager()
+
+    def tearDown(self):
+        pass
+
+    def test_courseAdd(self):
+        pass
