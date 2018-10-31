d24c33541598a50ff3b8067c7222a7007f5dffbd
Tichking09 <Tichking09@gmail.com>
2018-10-31 00:11:45 -0500

    updated

---
+++ sectionTests.py
@@ -0,0 +1,34 @@
+import unittest
+
+from sectionManager import Section
+
+class sectionTest(unittest.TestCase):
+
+    def setup(self):
+        set = Section("CS", "251", "401")
+
+
+    # test if calling to add a section that's time conflicts with its lecture fails
+    def test_timeConflict(self):
+        pass
+
+    # make sure you can't remove a lecture section that has discussions and lab sections attached
+    def test_removeLecture(self):
+        pass
+
+    # test to make sure adding a section without a lecture can't be done
+    def test_discDependency(self):
+        pass
+
+    # test that adding a new section with a TA/instructor does not create a conflict in the TA/instructors schedule
+    def test_scheduleConflict(self):
+        pass
+
+    # make sure "section delete 801" can only delete a disc/lab section and doesn't delete anything else
+    def test_delete(self):
+        pass
+
+    # test "section view secNum" command output
+    def test_view(self):
+        pass
+
