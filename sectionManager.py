05b89b821cb58f56ab04167c29741f390998b38f
Sams <tyczkow4@uwm.edu>
2018-11-01 17:43:18 -0500

    update tests and manager

--- sectionManager.py
+++ sectionManager.py
@@ -1,6 +1,8 @@
+from StorageManager import JSONStorageManager as jsm
+import abc
 
 
-class sectionManager:
+class SectionManager(abc.ABC):
 
     # validates and adds to database if okay
     def add(self, dept=None, cnum=None, snum=None, ins=None):
@@ -18,3 +20,97 @@ class sectionManager:
     def edit(self, dept=None, cnum=None, snum=None, ins=None):
         pass
 
+
+class Section(SectionManager):
+
+    # validates and adds to database if okay
+    def add(self, dept=None, cnum=None, snum=None, ins=None):
+
+        """Don't know what file to write to"""
+        db = jsm.JSONStorageManager("section.json")
+
+        # check if user inputs information needed for adding
+        invalid = self.actionHelper(dept, cnum, snum, "addition")
+        if invalid != "okay":
+            return invalid
+        if self.exists(db, dept, cnum, snum):
+            return "Section already exists"
+
+        db.insert_section(self)
+
+        """Do you prefer to create a string prior to return or is this okay?"""
+        return "Section Added: " + dept + "-" + cnum + "-" + snum
+        pass
+
+    # validates and deletes from database
+    def delete(self, dept=None, cnum=None, snum=None, ins=None):
+        pass
+
+    # validates and copies information from database to report
+    def view(self, dept=None, cnum=None, snum=None, ins=None):
+        invalid = self.actionHelper(dept, cnum, snum, "view")
+        if invalid != "okay":
+            return invalid
+        db = jsm.JSONStorageManager("section.json")
+        result = db.get_section(dept, cnum, snum)
+        if result is None:
+            return "Could not find" + dept + "-" + cnum + "-" + snum
+        else:
+            return result
+
+    # validates and takes given section and edits what is asked to edit
+    def edit(self, dept=None, cnum=None, snum=None, ins=None):
+        pass
+
+    # make sure necessary fields are not set to None
+    def actionHelper(self, dept, cnum, snum, action):
+        okay = ""
+        switch = {
+            dept: "Could not complete " + action + ", department is needed",
+            cnum: "Could not complete " + action + ", course number is needed",
+            snum: "Could not complete " + action + ", section number is needed"
+        }
+        return switch.get(None, "okay")
+
+    # get associated instructor/TA
+    # helper is used to make sure adding a TA/instructor to a section doesn't create a conflict
+    def getInstructor(self, snum, course, department):
+        pass
+
+    # check if number format is correct (e.g. labs should be 801, discussions 601, lectures 401)
+    def valNum(self):
+        pass
+
+    # make sure lectures don't conflict with discussion/lab times
+    def valTime(self):
+        pass
+
+    # make sure the person being assigned to a section's time doesn't conflict when added or edited
+    def valUserSchedule(self):
+        pass
+
+    # return true if section already exists for this course
+    def exists(self, db, dept, cnum, snum):
+        retrieved = db.get_section(dept, cnum, snum)
+        if retrieved is None:
+            return False
+        else:
+            return True
+
+    def getDepartment(self):
+        return self.department
+
+    def getCourse(self):
+        return self.course
+
+    def getSectionNum(self):
+        return self.sectionNum
+
+    def setDepartment(self, dept):
+        self.department = dept
+
+    def setCourse(self, course):
+        self.course = course
+
+    def setSectionNum(self, snum):
+        self.sectionNum = snum
--- sectionTests.py
+++ sectionTests.py
@@ -1,7 +1,7 @@
 import unittest
 from courseManager import CourseManager
-from sectionManager import sectionManager
-from userManager import UserManager
+from sectionManager import SectionManager
+from user_Manager import UserManager
 
 class sectionTest(unittest.TestCase):
 
@@ -11,14 +11,20 @@ class sectionTest(unittest.TestCase):
         self.user.add("Bob", "Instructor")
         self.user.add("Rob", "TA")
         self.user.add("Randall Cobb", "Instructor")
-        self.sec = sectionManager()
+        self.course.add(dept="CS", cnum="351", ta="Bob", secton="401")
+        self.sec = SectionManager()
 
     def test_add(self):
         self.assertEquals(self.sec.add("CS", "251", "401"), "Section 401 added to CS-251")
 
     def test_addNoInfo(self):
-        self.assertEquals(self.sec.add(dept="CS", cnum="251"), "No section was specified")
-        self.assertEquals(self.sec.add(dept="CS", cnum="251", ins="Bob"), "No section was specified")
+        self.assertEquals(self.sec.add(dept="CS", cnum="251"), "Could not complete addition, section is needed")
+        self.assertEquals(self.sec.add(dept="CS", cnum="251", ins="Bob"), "Could not complete addition, section"
+                                                                          " is needed")
+        self.assertEquals(self.sec.add(dept="CS", snum="401", ins="Bob"), "Could not complete addition, "
+                                                                          "course is needed")
+        self.assertEquals(self.sec.add(cnum="251", snum="401", ins="Bob"), "Could not complete addition, "
+                                                                           "department is needed")
 
     def test_notQualified(self):
         self.assertEquals(self.sec.add(dept="CS", cnum="251", snum="401", ins="Rob"), "Rob can't teach lectures")
@@ -28,14 +34,9 @@ class sectionTest(unittest.TestCase):
         self.assertEquals(self.sec.add("CS", "251", "401"), "Section 401 already exists in CS-251")
 
     # test if calling to add a section that's time conflicts with its lecture fails
+    """No time has been implemented yet"""
     def test_timeConflict(self):
         pass
-
-    # test to make sure adding a section without a lecture can't be done
-    def test_discDependency(self):
-        self.assertEquals(self.sec.add("CS", "251", "801"), "CS-251 needs a lecture section before adding "
-                                                            "disc/lab section")
-
 
     # make sure "section delete 801" can only delete a disc/lab section and doesn't delete anything else
     def test_delete(self):
@@ -45,8 +46,9 @@ class sectionTest(unittest.TestCase):
                                                                "because a discussion/lab depends on it")
 
     # test "section view secNum" command output
+    """I don't know if the output is correct"""
     def test_view(self):
-        pass
+        self.assertEquals(self.sec.view("CS", "351", "401"), "Instructor=Bob")
 
     def test_edit(self):
         self.sec.add("CS", "251", "401", "Rob")
