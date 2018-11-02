f1e96cc894b69b13132dc2ca8bf2dd27c95ba053
Sams <tyczkow4@uwm.edu>
2018-11-01 20:23:17 -0500

    changed up some methods and added toString() to Section

--- sectionManager.py
+++ sectionManager.py
@@ -41,8 +41,7 @@ class mySectionManager(SectionManager):
         db.insert_section(sec)
 
         """Do you prefer to create a string prior to return or is this okay?"""
-        return "Section Added: " + dept + "-" + cnum + "-" + snum
-        pass
+        return "Section Added: " + sec.toString()
 
     # validates and deletes from database
     def delete(self, dept=None, cnum=None, snum=None, ins=None):
@@ -50,16 +49,19 @@ class mySectionManager(SectionManager):
 
     # validates and copies information from database to report
     def view(self, dept=None, cnum=None, snum=None, ins=None):
+
+        """check if user has input enough information"""
         invalid = self.actionHelper(dept, cnum, snum, "view")
         if invalid != "okay":
             return invalid
+
+        """Check if """
         db = jsm.JSONStorageManager("section.json")
         result = db.get_section(dept, cnum, snum)
         if result is None:
             return "Could not find" + dept + "-" + cnum + "-" + snum
         else:
-
-            return result
+            return result.toString()
 
     # validates and takes given section and edits what is asked to edit
     def edit(self, dept=None, cnum=None, snum=None, ins=None):
@@ -75,20 +77,17 @@ class mySectionManager(SectionManager):
         }
         return switch.get(None, "okay")
 
-    # get associated instructor/TA
-    # helper is used to make sure adding a TA/instructor to a section doesn't create a conflict
-    def getInstructor(self, snum, course, department):
-        pass
-
     # check if number format is correct (e.g. labs should be 801, discussions 601, lectures 401)
     def valNum(self):
         pass
 
     # make sure lectures don't conflict with discussion/lab times
+    """Times aren't implemented yet"""
     def valTime(self):
         pass
 
     # make sure the person being assigned to a section's time doesn't conflict when added or edited
+    """times aren't implemented yet"""
     def valUserSchedule(self):
         pass
 
@@ -125,3 +124,6 @@ class Section:
 
     def setSectionNum(self, snum):
         self.sectionNum = snum
+
+    def toString(self):
+        return self.department + "-" + self.courseNum + "-" + self.sectionNum
