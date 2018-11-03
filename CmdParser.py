e8d01877ac5c39a8a9894ceef60246418e5ec557
Max Lowery <Sass-Meister@users.noreply.github.com>
2018-11-03 16:34:42 -0500

    Make the course command work

--- CmdParser.py
+++ CmdParser.py
@@ -1,7 +1,7 @@
-from user_manager   import UserManager    as UM
+from user_manager import UserManager as UM
 from sectionManager import sectionManager as SM
-from courseManager  import CourseManager  as CM
-from auth_manager   import AuthManager    as AM
+from courseManager import CourseManager  as CM
+from auth_manager import AuthManager as AM
 from user_manager   import User
 
 # This is the command parser class, used to bridge the gap between the CLI and our managers so the managers are able to
@@ -16,19 +16,24 @@ from user_manager   import User
 # testing with the auth manager). Hopefully this does not break any scrum/agile stuff. The testing field could be a huge
 # security flaw if left in but for now it's my best solution.
 
+# TODO:
+# Implement rest of commands
+# Implement uting (so this can be actually tested)
+# Make sure the usage of managers is correct (or is that for code reviews)
+
 
 class CommandParser:
     # These two data sets contain the all the commands and their description. The first array is useful for testing if
     # the parse function has been passed a valid command and the descriptions are useful for help and telling the user
     # when they messed up a command call.
     commandList = ['login', 'logout', 'course', 'section', 'user', 'help', 'exit']
-    descriptionList = {'login': "",
-                       'logout': "",
-                       'course': "",
-                       'section': "",
-                       'user': "",
-                       'help': "",
-                       'exit': ""}
+    descriptionList = {'login': "Placeholder description for login",
+                       'logout': "Placeholder description for logout",
+                       'course': "Placeholder description for course",
+                       'section': "Placeholder description for section",
+                       'user': "Placeholder description for user",
+                       'help': "Placeholder description for help",
+                       'exit': "Placeholder description for exit"}
 
     # This dictionary contains the a set of key values pairs where the key is the command name and the value is the
     # class the handles all that. It contains department because im still not exactly clear how we're going to deal with
@@ -39,13 +44,16 @@ class CommandParser:
                  "section": "sectionManager",
                  "department": "DepartmentManger"}
 
+    # The only field is an optional set to True if the class is being unit tested. If set to true, parse will return the
+    # command calls and the params as a string.
     def __init__(self, uting: bool = False):
         self.user = None
         self.uting = uting
         self.commandList = CommandParser.commandList
 
-        self.authmgr = AM()
+        self.authmgr = AM() # Only these managers need to be initialized (for some reason)
         self.usermgr = UM()
+        self.coursemgr = CM()
 
     def currentUser(self):
         return self.user
@@ -58,7 +66,7 @@ class CommandParser:
         cmdarr[0] = cmdarr[0].lower()
 
         if(len(cmdarr) == 0 or not (cmdarr[0] in CommandParser.commandList)):
-            return "Invalid Command"
+            return "Command not found"
 
         elif(cmdarr[0] == 'course'):
             return self.parseCourse(command)
@@ -90,13 +98,53 @@ class CommandParser:
 
             return "Logged out"
 
+        elif(cmdarr[0] == 'help'):
+            if(cmdarr[1].lower() in CommandParser.descriptionList):
+                return CommandParser.descriptionList[cmdarr[1].lower()]
+            return "Could not find that command"
+
         else:
-            return "Invalid command"
+            return "Invalid Command"
 
     # A helper method to just be used inside this class to parse one of the major commands, course. This also checks
     # auth to insure that the user has perms to do what they're trying to do.
     def parseCourse(self, command:str)->str:
-        return ""
+        reqFields = ['dept', 'cnum']
+        optFields = ['ta', 'snum']
+
+        cmdarr = command.split(" ")
+        if(len(cmdarr) < 3):
+            return "Invalid use"
+
+        command = cmdarr[0].lower()
+        action = cmdarr[1].lower()
+        fields = CommandParser.fieldsToDict(cmdarr[2:])
+
+        for f in reqFields: # If a required field is not in the given fields
+            if f not in fields:
+                return "Missing field: %s" % f
+
+        for f in optFields: # Replacing optional fields with a None so that function can be called without error
+            if f not in fields:
+                fields[f] = None
+
+        if(not command == 'course'):
+            return "Something went wrong!"
+
+        if(action == 'add'):
+            self.coursemgr.add(dept=fields['dept'], cnum=fields['cnum'], ta=fields['ta'], section=fields['snum'])
+
+        elif(action == 'edit'):
+            self.coursemgr.edit(dept=fields['dept'], cnum=fields['cnum'], ta=fields['ta'], section=fields['snum'])
+
+        elif(action == 'view'):
+            self.coursemgr.view(dept=fields['dept'], cnum=fields['cnum'], ta=fields['ta'], section=fields['snum'])
+
+        elif(action == 'delete' or action == 'remove'):
+            self.coursemgr.delete(dept=fields['dept'], cnum=fields['cnum'], ta=fields['ta'], section=fields['snum'])
+
+        else:
+            return "Incorrect use"
 
     # A helper method to just be used inside this class to parse one of the major commands, user. This also checks
     # auth to insure that the user has perms to do what they're trying to do.
@@ -107,3 +155,37 @@ class CommandParser:
     # auth to insure that the user has perms to do what they're trying to do.
     def parseSection(self, command:str)->str:
         return ""
+
+    # Another helper method to convert to help convert the fields to be passed to the managers. If the given list would
+    # have two different values for one key only the last one will be saved. If the given array contains a code it must
+    # come first
+    @staticmethod
+    def fieldsToDict(arr: list, delimiter: str = '=')->dict:
+        rtr = {}
+
+        if(("code%s"%delimiter) in arr[0]):
+            rtr = CommandParser.codeToDict(arr[0]);
+
+        for entry in arr:
+            if(not ("code%s"%delimiter) in entry):
+                kvpair = entry.split(delimiter)
+
+                if(len(kvpair) > 1):
+                    rtr[kvpair[0]] = kvpair[1]
+
+        return rtr
+
+    # Hopefully the last helper method, used to convert a code to a dictionary of it's parts
+    # EX: "code=CS-351-601" -> {'dnum': 'CS', 'cnum': '351', 'snum': '601'}
+    @staticmethod
+    def codeToDict(code: str)->dict:
+        cArr = code.split("-")
+        rtr = {'dept': cArr[0][5:]} # the [5:] removes the code= part
+
+        if(len(cArr) > 1):
+            rtr['cnum'] = cArr[1]
+
+        if(len(cArr) > 2):
+            rtr['snum'] = cArr[2]
+
+        return rtr
