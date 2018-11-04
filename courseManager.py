ef5f437139e7d5d27ec71929abfbba617b87fca8
Jas <singh87@uwm.edu>
2018-11-04 01:28:23 -0500

    Updated skeleton class

--- courseManager.py
+++ courseManager.py
@@ -1,13 +1,33 @@
+from StorageManager.JSONStorageManager import JSONStorageManager
+from sectionManager import mySectionManager
+
+class Course:
+    
+    def __init__(self,dept,cnum,ta,section):
+        self.dept = dept
+        self.cnum = cnum
+        self.ta = ta
+        self.section = section
+        
+
 class CourseManager:
 
-    def add(self, dept=None, cnum=None, ta=None, secton=None):
+    depts = ['CS']
+    c = None
+    s = JSONStorageManager()
+    sec = mySectionManager()
+    
+
+    def add(self, dept=None, cnum=None, ta=None, section=None, instr=None):
+        pass
+    
+    def view(self, dept=None, cnum=None, ta=None, section=None, instr=None):
         pass
 
-    def delete(self,dept=None, cnum=None, ta=None, secton=None):
+    def delete(self,dept=None, cnum=None, ta=None, section=None, instr=None):
         pass
 
-    def edit(self,dept=None, cnum=None, ta=None, secton=None):
+    def edit(self,dept=None, cnum=None, ta=None, section=None, instr=None):
         pass
         
-    def view(self, dept=None, cnum=None, ta=None, secton=None):
-        pass     
+  
