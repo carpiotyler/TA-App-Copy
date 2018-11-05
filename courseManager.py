15b15a0ea47174f99547f0fe1978f9ca263edf31
Jas <singh87@uwm.edu>
2018-11-04 21:26:06 -0600

    Changed add and view params

--- courseManager.py
+++ courseManager.py
@@ -1,13 +1,9 @@
 from StorageManager.JSONStorageManager import JSONStorageManager
+from StorageManager.myStorageManager import AbstractStorageManager as storage
 from sectionManager import mySectionManager
 
 # Course obj used for CourseManger, might place in seperate file once we finalize everything
-class Course:
-    
-    def __init__(self,dept,cnum,ta):
-        self.dept = dept
-        self.cnum = cnum
-        self.ta = ta
+
         
 # Handles adding,viewing,editing and deleting of all courses.
 class CourseManager:
@@ -16,44 +12,59 @@ class CourseManager:
     depts = ['CS']
     c = None    
     s = JSONStorageManager()
+    s.set_up()
     sec = mySectionManager()
     
 
     # Adds course to database using database manager and section manager
-    def add(self, dept=None, cnum=None, ta=None, section=None, instr=None):
+    def add(self, dept=None, cnum=None, instr=None, section=None):
 
         # Both dept and cnum are mandatory in order to add course
-        if dept is None: raise ValueError ("Dept not specified")
-        if cnum is None: raise ValueError ("Cnum not specified")
-    
+        if not self._check_params(dept,cnum,instr,section): return
         dept = dept.upper()
 
-        # Messy if statements, need to get refactored in next commit
-        if dept in self.depts: 
-            if cnum.isdigit():
-                if isinstance(ta,str):
-                    c = Course(dept,cnum,ta)
-                    self.s.insert_course(c)
-                else:
-                    c = Course(dept,cnum,None)
-                    self.s.insert_course(c)
-        else: raise ValueError ("Invalid dept.")
+        if dept in self.depts:
+            if section is None:
+                c = storage.Course(dept,cnum,[],'','')
+                self.s.insert_course(c)
+            else:
+                c = storage.Course(dept,cnum,[],'','')
+                self.s.insert_course(c)
+                self.sec.add(dept,cnum,section,instr)
 
+        else: 
+#            print("Invalid dept.")
+            return False
         # If parser provides section, call the section manager to add it.
         if section!=None:
             self.sec.add(dept,cnum,section,instr)
     
     # Get course from database manager to pass back to the parser in order to print
-    # Will need to change depending on the format that the parser requires
-    def view(self, dept=None, cnum=None, ta=None, section=None, instr=None):
-       
-        if dept is None: raise ValueError ("Dept not specified")
-        if cnum is None: raise ValueError ("Cnum not specified")
-        self.s.get_course(dept,cnum)
+    def view(self, dept=None, cnum=None, instr=None, section=None):
+       if self._check_params(dept,cnum):
+           return self.s.get_course(dept,cnum)
 
-    def delete(self,dept=None, cnum=None, ta=None, section=None, instr=None):
+    def delete(self,dept=None, cnum=None, instr=None, section=None ):
         pass
 
-    def edit(self,dept=None, cnum=None, ta=None, section=None, instr=None):
+    def edit(self,dept=None, cnum=None, instr=None, section=None ):
         pass
         
+    def _check_params(self=None,dept=None,cnum=None,instr=None,section=None):
+        if not dept or None: 
+#            print( "Dept not specified.") 
+            return False
+        if not cnum or None: 
+#            print("Cnum not specified.") 
+            return False
+        if not cnum.isdigit(): 
+#            print("Invalid Cnum.") 
+            return False
+        if instr:
+            if not all(x.isalpha() or x.isspace() for x in instr):
+#               print("Instructor name should only contain letters.")
+                return False
+        if instr and not section: 
+#            print("Instructor must have section") 
+            return False
+       
