8a710d1098899bcd3add11ffd2862472921545ef
singh87 <singh87@uwm.edu>
2018-11-05 00:11:06 -0600

    updated tests

--- tests/courseTests.py
+++ tests/courseTests.py
@@ -1,21 +1,21 @@
 import unittest
-from courseManager import CourseManager
+from Managers.courseManager import CourseManager
 
 class CourseTest(unittest.TestCase):
     def setUp(self):
         self.c = CourseManager()
 
-        self.c.add(dept='CS',cnum='351',ta='Joe')
-        self.c.add(dept='CS',cnum='341',ta='Alice')
-        self.c.add(dept='CS',cnum='331',ta='Yang')
+        self.c.add(dept='CS',cnum='351',instr='Joe')
+        self.c.add(dept='CS',cnum='341',instr='Alice')
+        self.c.add(dept='CS',cnum='331',instr='Yang')
 
     def tearDown(self):
         pass
 
     def test_course_add(self):
 
-        self.assertEquals("Successfully Added Course",self.c.add(dept='CS',cnum='359',ta='Jason'))
-        self.assertEquals("Successfully Added Course",self.c.add(dept='CS',cnum='359'))
+        self.assertEqual(False,self.c.add(dept='CS',cnum='359',instr='Jason'),"Cannot add Intructor with no section")
+        self.assertEqual(True,self.c.add(dept='CS',cnum='359'),"Added Successfully")
 
     def test_course_add_fail(self):
 
@@ -24,9 +24,9 @@ class CourseTest(unittest.TestCase):
 
     def test_course_view(self):
 
-        self.assertEquals("CS-351, TA: Joe",self.c.view(dept='CS',cnum='351'))
-        self.assertEquals("CS-331, TA: Yang",self.c.view(dept='CS',cnum='331'))
-        self.assertEquals("CS-351, TA: Joe \nCS-341, TA: Alice \nCS-331, TA: Yang",self.c.view(dept='CS'))
+        self.assertEquals("CS-351, Instructor: Joe",self.c.view(dept='CS',cnum='351'))
+        self.assertEquals("CS-331, Instructor: Yang",self.c.view(dept='CS',cnum='331'))
+        self.assertEquals("CS-351, Instructor: Joe \nCS-341, Instructor: Alice \nCS-331, Instructor: Yang",self.c.view(dept='CS'))
 
     def test_course_view_no_course(self):
 
@@ -35,28 +35,28 @@ class CourseTest(unittest.TestCase):
 
     def test_course_view_all(self):
   
-        self.assertEquals("CS-351, TA: Joe \nCS-341, TA: Alice \nCS-331, TA: Yang",self.c.view())
+        self.assertEquals("CS-351, Instructor: Joe \nCS-341, Instructor: Alice \nCS-331, Instructor: Yang",self.c.view())
 
     def test_course_edit(self):
         
-        self.assertEquals("Edit Successful",self.c.edit(dept='CS',cnum='351',ta='None'))
+        self.assertEquals("Edit Successful",self.c.edit(dept='CS',cnum='351',instr='None'))
 
-        self.assertEquals("CS-351, TA: None \nCS-341, TA: Alice \nCS-331, TA: Yang",self.c.view())
+        self.assertEquals("CS-351, Instructor: None \nCS-341, Instructor: Alice \nCS-331, Instructor: Yang",self.c.view())
 
     def test_course_edit_unsuccessful(self):
         
-        self.assertEquals("Edit Unsuccessful, course not found",self.c.edit(dept='CS',cnum='361',ta='None'))
-        self.assertEquals("CS-351, TA: Joe \nCS-341, TA: Alice \nCS-331, TA: Yang",self.c.view())
+        self.assertEquals("Edit Unsuccessful, course not found",self.c.edit(dept='CS',cnum='361',instr='None'))
+        self.assertEquals("CS-351, Instructor: Joe \nCS-341, Instructor: Alice \nCS-331, Instructor: Yang",self.c.view())
 
     def test_course_delete(self):
 
         self.c.delete(dept='CS',cnum='341')
 
-        self.assertEquals("CS-351, TA: Joe \nCS-331, TA: Yang",self.c.view())
+        self.assertEquals("CS-351, Instructor: Joe \nCS-331, Instructor: Yang",self.c.view())
 
     def test_course_delete_unsuccessful(self):
 
         self.c.delete(dept='CS',cnum='311')
 
-        self.assertEquals("CS-351, TA: Joe \nCS-341, TA: Alice \nCS-331, TA: Yang",self.c.view())
+        self.assertEquals("CS-351, Instructor: Joe \nCS-341, Instructor: Alice \nCS-331, Instructor: Yang",self.c.view())
 
