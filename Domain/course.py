199a69093db9f4a857d999856fa7d52ae9fab640
Jas <singh87@uwm.edu>
2018-11-06 20:55:25 -0600

    More merge conflicts

--- Domain/course.py
+++
@@ -1,8 +0,0 @@
-class Course:
-
-    def __init__(self, str_dept, str_cnum, strarray_sections=[], str_name="", str_description=""):
-        self.dept = str_dept
-        self.cnum = str_cnum
-        self.sections = strarray_sections
-        self.name = str_name
-        self.description = str_description
