cbed623728bcfb1aba596076f5fc0c2d10dc4645
Tichking09 <Tichking09@gmail.com>
2018-11-20 16:47:52 -0600

    domain object section updated

--- Domain/section.py
+++ Domain/section.py
@@ -1,7 +1,11 @@
 class Section:
 
-    def __init__(self, str_dept, str_cnum, str_snum, str_instructor=""):
+    def __init__(self, str_dept, str_cnum, str_snum, str_instructor="", type=None, days=None, room=None, time=None):
         self.dept = str_dept
         self.cnum = str_cnum
         self.snum = str_snum
         self.instructor = str_instructor
+        self.type = type
+        self.days = days
+        self.room = room
+        self.time = time
