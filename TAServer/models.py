67894a520598c0833315d04bc72d81ae7ee90d7e
Max Lowery <Sass-Meister@users.noreply.github.com>
2018-11-26 12:44:19 -0600

    Added the permission groups to models

--- TAServer/models.py
+++ TAServer/models.py
@@ -3,20 +3,78 @@ from django.contrib.auth.models import User, Group
 from django.db import models
 
 
+class DefaultGroup(Group):
+    permissions = (("can_view_courses", "Can view courses"),
+
+                   ("can_view_sections", "Can view sections"),
+
+                   ("can_view_user", "Can view users"))
+
+
 class TAGroup(Group):
-    pass
+    permissions = (("can_view_courses", "Can view courses"),
+
+                   ("can_view_sections", "Can view sections"),
+
+                   ("can_edit_self", "Can edit users"),
+                   ("can_view_user", "Can view users"))
 
 
 class InsGroup(Group):
-    pass
+    permissions = (("can_view_courses", "Can view courses"),
+
+                   ("can_edit_self", "Can edit users"),
+                   ("can_view_user", "Can view users"),
+                   ("can_view_private", "Can view private user data"),
+
+                   ("can_assign_ta", "Can assign TA's"),
+
+                   ("can_email_tas", "Can send emails"))
 
 
 class AdminGroup(Group):
-    pass
+    permissions = (("can_create_course", "Can create courses"),
+                   ("can_edit_courses", "Can edit courses"),
+                   ("can_delete_courses", "Can delete courses"),
+                   ("can_view_courses", "Can view courses"),
+
+                   ("can_create_sections", "Can create sections"),
+                   ("can_edit_sections", "Can edit sections"),
+                   ("can_delete_sections", "Can delete sections"),
+                   ("can_view_sections", "Can view sections"),
+
+                   ("can_create_user", "Can create users"),
+                   ("can_edit_user", "Can edit users"),
+                   ("can_edit_self", "Can edit users"),
+                   ("can_delete_user", "Can delete users"),
+                   ("can_view_user", "Can view users"),
+                   ("can_view_private", "Can view private user data"),
+
+                   ("can_email_all", "Can send emails to all users"))
 
 
 class SupGroup(Group):
-    pass
+    permissions = (("can_create_course", "Can create courses"),
+                   ("can_edit_courses", "Can edit courses"),
+                   ("can_delete_courses", "Can delete courses"),
+                   ("can_view_courses", "Can view courses"),
+
+                   ("can_create_sections", "Can create sections"),
+                   ("can_edit_sections", "Can edit sections"),
+                   ("can_delete_sections", "Can delete sections"),
+                   ("can_view_sections", "Can view sections"),
+
+                   ("can_create_user", "Can create users"),
+                   ("can_edit_user", "Can edit users"),
+                   ("can_edit_self", "Can edit users"),
+                   ("can_delete_user", "Can delete users"),
+                   ("can_view_user", "Can view users"),
+                   ("can_view_private", "Can view private user data"),
+
+                   ("can_assign_ta", "Can assign TA's"),
+                   ("can_assign_ins", "Can assign instructors"),
+
+                   ("can_email_all", "Can send emails to all users"))
 
 
 class Course(models.Model):
