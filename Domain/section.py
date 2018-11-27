6a38f4fcfdd761d3dd3e0673999610ece359f3a0
singh87 <singh87@uwm.edu>
2018-11-27 15:16:18 -0600

    Delete section.py

--- Domain/section.py
+++
@@ -1,7 +0,0 @@
-class Section:
-
-    def __init__(self, str_dept, str_cnum, str_snum, str_instructor=""):
-        self.dept = str_dept
-        self.cnum = str_cnum
-        self.snum = str_snum
-        self.instructor = str_instructor
