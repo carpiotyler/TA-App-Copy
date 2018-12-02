cd7afbad6de93055a25133bfceace8b222952cc8
Tyler Carpio <tkcarpio@uwm.edu>
2018-12-01 19:38:01 -0600

    Fixed userManager to a point; We have to define how view() in UM knows what fields are "viewable" given a logged in user.

--- Managers/userManager.py
+++ Managers/userManager.py
@@ -13,36 +13,40 @@ class UserManager(ManagerInterface):
     # This method simply adds a user to the database if they don't exist already.
     # Returns true if a new user was added, returns false otherwise
     def add(self, fields: dict) -> bool:
-        print(fields)
-        user = self.storage.get_user(fields.get("username"))
+        # Need username and password to add a user!
+        if 'username' not in fields.keys(): return False
 
-        if user is None:  # user dne
+        user = self.storage.get_user(fields["username"])
+        if user is None:  # user dne!
             user = User()
-            user.username = "" if fields.get("username") is None else fields.get("username")
-            user.address = "" if fields.get("address") is None else fields.get("address"),
-            user.phonenum = "" if fields.get("phone_number") is None else fields.get("phone_number")
-            user.email = "" if fields.get("email") is None else fields.get("email")
-            user.password = "" if fields.get("password") is None else fields.get("password")
-            user.role = "" if fields.get("role") is None else fields.get("role")
+            user.username = fields["username"]
+
+            # if fields['role'] is either None or "", set fields['role'] to 'default'
+            # if fields['role'] exists and is invalid, return false. Otherwise, set role as fields['role']
+            if 'role' not in fields.keys() or fields['role'] is None or fields['role'].strip() == "":
+                user.role = 'default'
+            elif fields['role'] in dict(User.ROLES).values():
+                user.role = fields['role']
+            else:
+                return False
+
+            # Unvalidated fields...
+            if 'password' in fields.keys() and fields['password'] is not None and len(
+                fields['password'].strip()) > 0: user.password = fields['password']
+            if 'phonenum' in fields.keys() and fields['phonenum'] is not None and len(
+                fields['phonenum'].strip()) > 0: user.phonenum = fields['phonenum']
+            if 'address' in fields.keys() and fields['address'] is not None and len(
+                fields['address'].strip()) > 0: user.phonenum = fields['address']
+            if 'firstname' in fields.keys() and fields['firstname'] is not None and len(
+                fields['firstname'].strip()) > 0: user.phonenum = fields['firstname']
+            if 'lastname' in fields.keys() and fields['lastname'] is not None and len(
+                fields['lastname'].strip()) > 0: user.phonenum = fields['lastname']
+            if 'email' in fields.keys() and fields['email'] is not None and len(
+                fields['email'].strip()) > 0: user.phonenum = fields['email']
+            if 'bio' in fields.keys() and fields['bio'] is not None and len(
+                fields['bio'].strip()) > 0: user.phonenum = fields['bio']
+
             self.storage.insert_user(user)
-
-            # if user.role is None or user.role == "default":
-            #     user.groups.add(DefaultGroup())
-            #
-            # elif user.role == "ta":
-            #     user.groups.add(TAGroup())
-            #
-            # elif user.role == "ins":
-            #     user.groups.add(InsGroup())
-            #
-            # elif user.role == "admin":
-            #     user.groups.add(AdminGroup())
-            #
-            # elif user.role == "sup":
-            #     user.groups.add(SupGroup())
-            # else:
-            #     return false
-
             return True
 
         else:  # user exists
@@ -53,70 +57,85 @@ class UserManager(ManagerInterface):
     # Will only print out fields given by the input fields dict (for permission obedience)
     # A View that returns a string of multiple users outputs sorted by username
     # String is newlined for each user field, double newlined for every new user displayed
-    def view(self, fields) -> str:
-        if fields.get("username") is None:  # fetch all users
-            users = self.storage.get_users_by()
-            if len(users) == 0:
-                return "No Users"
-            else:
-                return '|'.join(map(lambda x: x.__str__(), users))
 
-        else:  # fetch single user
-            user = self.storage.get_user(fields.get("username"))
-            return "No User Available" if (user is None) else user.__str__()
+    # NOT FULLY FUNCTIONAL, WE NEED TO BE ABLE TO PASS IN PERMISSIONS TO VIEW CERTAIN FIELDS
+    def view(self, fields) -> str:
+        retval = ""
+
+        if 'username' in fields.keys() and fields['username'].strip() != "":
+            # We have to return just one user's view. ez
+            user = self.storage.get_user(fields['username'])
+            if user is None:
+                retval = 'No Users'
+            else:
+                retval = "" + user.username + "\n" + "role=" + user.role + "\n\n"
+
+            return retval.lower()
+        else:
+            # Return mutiple users. (We only support viewing all right now
+            users = self.storage.get_users_by()
+            users = list(users)
+            users.sort(key= lambda User: User.username)
+            for user in users:
+                retval += user.username + "\n" + "role=" + user.role + "\n\n"
+            return retval.lower()
 
     # This method edits an existing user
     # Returns true if a user existed (And therefore edited), or false if no user existed
     def edit(self, fields: dict) -> bool:
-        user = self.storage.get_user(fields.get("username"))
+        # Need username to edit a user!
+        if 'username' not in fields.keys(): return False
 
-        if user is None:  # user dne
-            return False
+        user = self.storage.get_user(fields["username"])
+        if user is not None:  # user exists
 
-        else:  # found user
-            user = User()
-            user.username = "" if fields.get("username") is None else fields.get("username")
-            user.address = "" if fields.get("address") is None else fields.get("address"),
-            user.phonenum = "" if fields.get("phone_number") is None else fields.get("phone_number")
-            user.email = "" if fields.get("email") is None else fields.get("email")
-            user.password = "" if fields.get("password") is None else fields.get("password")
-            user.role = "" if fields.get("role") is None else fields.get("role")
+            # if fields['role'] is either None or "", set fields['role'] to 'default'
+            # if fields['role'] exists and is invalid, return false. Otherwise, set role as fields['role']
+            if 'role' not in fields.keys() or fields['role'] is None or fields['role'].strip() == "":
+                user.role = 'default'
+            elif fields['role'] in dict(User.ROLES).values():
+                user.role = fields['role']
+            else:
+                return False
+
+            # Unvalidated fields...
+            if 'password' in fields.keys() and fields['password'] is not None and len(
+                fields['password'].strip()) > 0: user.password = fields['password']
+            if 'phonenum' in fields.keys() and fields['phonenum'] is not None and len(
+                fields['phonenum'].strip()) > 0: user.phonenum = fields['phonenum']
+            if 'address' in fields.keys() and fields['address'] is not None and len(
+                fields['address'].strip()) > 0: user.phonenum = fields['address']
+            if 'firstname' in fields.keys() and fields['firstname'] is not None and len(
+                fields['firstname'].strip()) > 0: user.phonenum = fields['firstname']
+            if 'lastname' in fields.keys() and fields['lastname'] is not None and len(
+                fields['lastname'].strip()) > 0: user.phonenum = fields['lastname']
+            if 'email' in fields.keys() and fields['email'] is not None and len(
+                fields['email'].strip()) > 0: user.phonenum = fields['email']
+            if 'bio' in fields.keys() and fields['bio'] is not None and len(
+                fields['bio'].strip()) > 0: user.phonenum = fields['bio']
+
             self.storage.insert_user(user)
-
-            if user.role is None or user.role == "default":
-                user.groups.add(DefaultGroup())
-
-            elif user.role == "ta":
-                user.groups.add(TAGroup())
-
-            elif user.role == "ins":
-                user.groups.add(InsGroup())
-
-            elif user.role == "admin":
-                user.groups.add(AdminGroup())
-
-            elif user.role == "sup":
-                user.groups.add(SupGroup())
-
             return True
+
+        else:  # user dne!
+            return False
 
     # Deletes a user if it exists.
     # Returns true if it did delete an existing user, false otherwise
     def delete(self, fields: dict) -> bool:
-        user = self.storage.get_user(fields.get("username"))
+        if 'username' not in fields.keys(): return False
 
-        if user is None:  # user dne
+        user = self.storage.get_user(fields['username'])
+        if user is None:
             return False
-        #
-        else:  # found user
-            # self.storage.delete_user(user)
+        else:
+            self.storage.delete(user)
             return True
-        pass
 
     @staticmethod
     def reqFields() -> list:
-        return ["username", "password"]
+        return ["username"]
 
     @staticmethod
     def optFields() -> list:
-        return ["role", "address", "phone_number", "email"]
+        return ["password", "role", "address", "phone_number", "email", "firstname", "lastname", "bio"]
--- Tests/unit_tests/test_userManager.py
+++ Tests/unit_tests/test_userManager.py
@@ -18,35 +18,40 @@ class UserManagerTests(TestCase):
                   'password':'123'}
 
         # Testing that the returns are the expected behavior:
-        self.assertTrue(self.user_manager.add(self, fields), "Should be a valid user add!")
-        self.assertFalse(self.user_manager.add(self, fields), "Should no longer be able to add !: User Exists")
+        self.assertTrue(self.user_manager.add(fields), "Should be a valid user add!")
+        self.assertFalse(self.user_manager.add(fields), "Should no longer be able to add !: User Exists")
 
     def test_user_add_incorrect_fields(self):
         # Completely incorrect add, the req fields aren't filled out!
         fields = {'un': 'new',
                   'pass': 'lola'}
-        self.assertFalse(self.user_manager.add(self, fields), "Should only add if reqfields are filled out!")
+        self.assertFalse(self.user_manager.add(fields), "Should only add if reqfields are filled out!")
+
+        fields = {'username': 'test',
+                  'password':'123',
+                  'role': 'student'}
+        self.assertFalse(self.user_manager.add(fields), "Role provided not in ROLES! Should not add!")
 
     def test_user_add_complicated(self):
         # A complicated, highest level test, shouldn't fail or throw exceptions:
         fields = {'username': 'test',
                   'password': '123',
-                  'role': User.ROLES["I"],  # Instructor, ins, whatever
+                  'role': dict(User.ROLES)["I"],  # Instructor, ins, whatever
                   'email':'foo@bar.com',
                   'address':'123 sesame street'}
 
         # Return val checking for subsequent calls
-        self.assertTrue(self.user_manager.add(self, fields), "Failed on omplicated user add")
-        self.assertFalse(self.user_manager.add(self, fields, "Shouldn't be able to call again"))
+        self.assertTrue(self.user_manager.add(fields), "Failed on omplicated user add")
+        self.assertFalse(self.user_manager.add(fields), "Shouldn't be able to call again")
 
     def test_view_user_basic(self):
         # adding basic user to test with
         fields = {'username': 'test',
                   'password': '123'}
 
-        self.assertEqual(self.user_manager.view({"username":"test"}), "No Users", "No users case!")
-        self.assertTrue(self.user_manager.add(self, fields))
-        self.assertEqual(self.user_manager.view(self, fields), "test\nrole=default\n\n")
+        self.assertEqual(self.user_manager.view({"username":"test"}), "no users", "No users case!")
+        self.assertTrue(self.user_manager.add(fields))
+        self.assertEqual(self.user_manager.view(fields), "test\nrole=default\n\n")
 
     def test_view_user_all_basic(self):
         # Adding three basic users to test with
@@ -55,19 +60,19 @@ class UserManagerTests(TestCase):
 
         fields2 = {'username': 'scotty',
                   'password': '232',
-                   'role':User.ROLES["A"]}
+                   'role':dict(User.ROLES)["A"]}
 
         fields3 = {'username': 'truff',
                   'password': 'pass',
-                   'role':User.ROLES["I"]}
+                   'role':dict(User.ROLES)["I"]}
 
-        self.assertTrue(self.user_manager.add(self, fields1), "Error adding fields1!")
-        self.assertTrue(self.user_manager.add(self, fields2), "Error adding fields2!")
-        self.assertTrue(self.user_manager.add(self, fields3), "Error adding fields3!")
+        self.assertTrue(self.user_manager.add(fields1), "Error adding fields1!")
+        self.assertTrue(self.user_manager.add(fields2), "Error adding fields2!")
+        self.assertTrue(self.user_manager.add(fields3), "Error adding fields3!")
 
         # View all (no username or role provided) must return all users, alphabetically sorted by username
-        self.assertEqual(self.user_manager.view(self, {}),
-                "scotty\nrole=administrator\n\nsup\nrole=supervisor\n\ntest\nrole=default\n\ntruff\nrole=instructor\n\n"
+        self.assertEqual(self.user_manager.view({}),
+                "scotty\nrole=administrator\n\nsupervisor\nrole=supervisor\n\ntest\nrole=default\n\ntruff\nrole=instructor\n\n"
                 , "This is a long, convoluted sting, but this is the expected output")
 
     def test_edit_user_basic(self):
@@ -75,35 +80,35 @@ class UserManagerTests(TestCase):
         fields = {'username': 'test',
                   'password': '123'}
 
-        self.assertFalse(self.user_manager.edit(self, fields, "No user to edit!"))
-        self.assertTrue(self.user_manager.add(self, fields), "Add should work")
+        self.assertFalse(self.user_manager.edit(fields), "No user to edit!")
+        self.assertTrue(self.user_manager.add(fields), "Add should work")
         # Editing nothing should still return true
-        self.assertTrue(self.user_manager.edit(self, fields), "Edit should return true if user exists! Even if no changes!")
+        self.assertTrue(self.user_manager.edit(fields), "Edit should return true if user exists! Even if no changes!")
         # Basic editing
         # Basic fields case
         fields = {'username': 'test',
                   'password': 'newpass'}
-        self.assertTrue(self.user_manager.edit(self, fields), "Edit should return true.")
+        self.assertTrue(self.user_manager.edit(fields), "Edit should return true.")
 
     def test_edit_user_roles_and_view_integrated(self):
         # Basic fields case
         fields = {'username': 'test',
                   'password': '123'}
 
-        self.assertTrue(self.user_manager.add(self, fields))
-        self.assertEqual(self.user_manager.view(self, {"username":"test"}), "test\nrole=default")
+        self.assertTrue(self.user_manager.add(fields))
+        self.assertEqual(self.user_manager.view({"username":"test"}), "test\nrole=default\n\n")
         # Basic fields case
         fields = {'username': 'test',
                   'password': '123',
-                  'role': User.ROLES['I']}
-        self.assertTrue(self.user_manager.edit(self, fields))
-        self.assertEqual(self.user_manager.view(self,{"username":"test"}), "test\nrole=instructor", "Edited!")
+                  'role': dict(User.ROLES)['I']}
+        self.assertTrue(self.user_manager.edit(fields))
+        self.assertEqual(self.user_manager.view({"username":"test"}), "test\nrole=instructor\n\n", "Edited!")
 
     def test_delete_user(self):
         # Basic fields case
         fields = {'username': 'test',
                   'password': '123'}
         self.assertTrue(self.user_manager.add(fields))
-        self.assertEqual(self.user_manager.view(self, {"username":"test"}), "test\nrole=default")
-        self.assertTrue(self.user_manager.delete(self, {"username":"test"}))
-        self.assertEqual(self.user_manager.view(self, {"username":"test"}), "No Users", "Should have been deleted!")
+        self.assertEqual(self.user_manager.view({"username":"test"}), "test\nrole=default\n\n")
+        self.assertTrue(self.user_manager.delete({"username":"test"}))
+        self.assertEqual(self.user_manager.view({"username":"test"}), "no users", "Should have been deleted!")
