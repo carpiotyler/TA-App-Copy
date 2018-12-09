f5a60a0cef799ed354ba323540d60e1fb5406918
Jas <singh87@uwm.edu>
2018-12-08 23:02:32 -0600

    Signup implemented

--- TAServer/views.py
+++ TAServer/views.py
@@ -1,6 +1,6 @@
 # This file is copied straight from Rock's provided code under "Skeleton code for Django" in sprint 2
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from Managers.userManager import UserManager as UM
 from Managers.sectionManager import SectionManager as SM
@@ -10,11 +10,22 @@ from Managers.DjangoStorageManager import DjangoStorageManager as Storage # Chan
 from TAServer.models import Staff as User
 from TAServer.models import DefaultGroup, TAGroup, InsGroup, AdminGroup, SupGroup
 from django.contrib.auth import authenticate, login, logout
+from TAServer.forms import SignUpForm
 
 
 def signup(request):
-    return render(request, "registration/signup.html")
-
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 class Home(View):
